1c055b0ccfddf0e700a39b878c04b0104cb63b14
a01737554-commits <a01737554@tec.mx>
2025-10-06 22:07:49 -0600

    Rama de maria

--- prueba1.py
+++ prueba1.py
@@ -1,1 +1,8 @@
 print("Hola")
+def es_primo(numero):
+    if numero < 2:
+        return False
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
